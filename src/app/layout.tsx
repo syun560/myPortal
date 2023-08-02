dac90f49f232b700446c1405b9249aaff9b375c4
syun560 <bajikoji@gmail.com>
2023-08-02 16:23:03 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
+  return(
     <html lang="en">
+      <head>
+        <meta charSet='utf-8' />
+        <title>syun560 page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"></Script>
+        <link rel="stylesheet" href="index.css" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,108 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <main className="bg-dark text-light">
+    <div className="container shadow mt-3">
+        <h1 className="border-bottom mb-3">syun560 page</h1>
+        <div className="shadow trans row">
+            <div className="col">
+                <h2>Profile</h2>
+                <ul>
+                    <li>python/Django, VR, music and programming</li>
+                </ul>
+                
+                <h2>Want to ...</h2>
+                <ul>
+                    <li>go to travel everywhere.</li>
+                    <li>read many Books...</li>
+                    <li>make smartphone app with React Native</li>
+                    <li>make MarkDown blog system</li>
+                    <li>edit movie</li>
+                    <li></li>
+                    <li>domain</li>
+                </ul>
+                
+                <h2>Daily Works</h2>
+                <ul>
+                    <li>Upload video</li>
+                    <li>WorkOut (Planking, etc..)</li>
+                </ul>
+
+                <h2>Google Sheets</h2>
+                <a href="https://sheets.googleapis.com/v4/spreadsheets/1XBS6iBZR1AR3aECSuqHLRUQ5zz--6j82WnGPUwTb1lw/values/masterpiece?key=AIzaSyCi75dWmDldMwJZAuS7N1QIRrGVGTH4m8I">api</a>
+                
+
+                <h2>Works</h2>
+                <table className="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">name(click to demo page)</th>
+                            <th scope="col">github</th>
+                            <th scope="col">keywords</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td><a href="https://syun560.github.io/midip2021/">MIDI Sequencer on browser</a></td>
+                            <td><a href="https://github.com/syun560/mp2022">GitHub</a></td>
+                            <td>
+                                <span className="badge bg-dark">React</span>
+                                <span className="badge bg-dark">TypeScript</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td><a href="https://gt-two.vercel.app/">Guitar Tablature Generator</a></td>
+                            <td><a href="https://github.com/syun560/gt">GitHub</a></td>
+                            <td>
+                                <span className="badge bg-dark">React</span>
+                                <span className="badge bg-dark">TypeScript</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>mahjoung assist tool</td>
+                            <td>GitHub</td>
+                            <td>
+                                <span className="badge bg-dark">React</span>
+                                <span className="badge bg-dark">TypeScript</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                <h2>Links</h2>
+                <ul>
+                    <li><a href="https://github.com/syun560/" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                    <li></li>
+                </ul>
+                
+                <h2>Music Room</h2>
+                <ul>
+                    <li>My First Song</li>
+                </ul>
+
+                <h2>Poems</h2>
+                <p>I do what I want to do. Life is short.</p>
+                <p>fight this situation...</p>
+
+            </div>
+            <div className="col">
+                <h2>Time To Live</h2>
+                <p id="RealtimeClockArea">00:00:00</p>
+                <h2>Tasks</h2>
+                <ul>
+                    <li>need more information</li>
+                </ul>
+                <h2>Schedule</h2>
+
+            </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <Script src="js/clock.js"></Script>
     </main>
   )
 }
